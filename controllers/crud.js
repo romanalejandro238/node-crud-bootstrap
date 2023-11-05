ebf8f48c69b9ed9807e7a27032d59d9300c23ba1
Roman Gaspar <romanalejandro99@hotmail.com>
2023-11-05 14:35:37 -0700

    problema de guardado en la edicion de el usuario resuelto

--- controllers/crud.js
+++ controllers/crud.js
@@ -15,7 +15,7 @@ exports.save = (req, res)=>{
 exports.update = (req,res)=>{
     const id = req.body.id
     const user = req.body.user
-    const rol = req.body.user
+    const rol = req.body.rol
 
     conexion.query('UPDATE users SET ? WHERE id=?',[{user:user,rol:rol},id], (error,results)=>{
         if(error){
